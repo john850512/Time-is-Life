336e793c17b5ef735062fec2425080a49bd9337b
john850512 <john85051232@gmail.com>
2017-08-05 18:10:43 +0800

    V2.02
    版本:V2.02
    日期:20170805
    說明:
    1.傳送"取樣點資訊"
    由於傳送太多點資訊會造成connect closed，原因不明，但是根據RFC表示可傳送大小理應很大
    為了解決只好透過取樣點資訊來傳送

--- Server/WebSocket/src/com/test/HelloWorldServlet.java
+++ Server/WebSocket/src/com/test/HelloWorldServlet.java
@@ -4,15 +4,18 @@ import java.io.IOException;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
+import javax.websocket.OnError;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/websocket")
 public class HelloWorldServlet {
 	public static StringBuilder allPath = new StringBuilder();
+	public static int path_num = 1;
     @OnMessage
     public void onMessage(String message, Session session) throws IOException, InterruptedException {
     	if(message.startsWith("gma:")){
-    		allPath.append("$1$"+message.split("gma:")[1]);
+    		allPath.append("$"+path_num+"$"+message.split("gma:")[1]);
+    		path_num++;
     	}
     	else{
             // Send the first message to the client
@@ -20,11 +23,9 @@ public class HelloWorldServlet {
     		session.getBasicRemote().sendText(allPath.toString()); 
     	}
         // Print the client message for testing purposes
+    	System.out.println("Received: " + message);
     	System.out.println("allpath: " + allPath.toString());
-        System.out.println("Received: " + message);
-
-
-
+        
     }
 
     @OnOpen
@@ -35,5 +36,9 @@ public class HelloWorldServlet {
     @OnClose
     public void onClose() {
         System.out.println("Connection closed");
+    }
+    @OnError
+    public void OnError(Session session, Throwable t) {
+        System.out.println("[error]"+t);
     }
 }
